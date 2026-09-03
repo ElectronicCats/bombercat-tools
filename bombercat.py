553c47748e14af536b89d411eb02be2783b7bd4c
Darcko123 <daniel18052002@yahoo.com>
2026-09-03 11:18:39 -0600

    refactor: remove vendor path setup from bombercat.py

--- bombercat.py
+++ bombercat.py
@@ -3,12 +3,6 @@
 import sys
 import os
 
-# Add vendor directory to sys.path for bundled dependencies
-# In development, this is relative to the current script
-vendor_path = os.path.join(os.path.dirname(__file__), "vendor")
-if os.path.exists(vendor_path):
-    sys.path.insert(0, vendor_path)
-
 # Internal
 from modules.core.cli import main_cli
 
